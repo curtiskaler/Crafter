b65cdcf60824ec063799a744ae6fff6b6baa13c7
Curtis Kaler <curt.kaler@gmail.com>
2026-08-29 14:40:19 -0700

    init - Remove Cruft

--- src/Auturge.Validation.Tests/TestResources/TestUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Auturge.Validation.Tests;
-
-public class TestUser(string? name, string? email = null, int? bones = null)
-{
-    public string? Name { get; set; } = name;
-    public string? Email { get; set; } = email;
-    public int? Bones { get; set; } = bones;
-    public string? Website { get; set; } = null;
-}
--- src/Auturge.Validation/Internal/Defaults.cs
+++
@@ -1,6 +0,0 @@
-namespace Auturge.Validation;
-
-internal static class Defaults
-{
-    internal static readonly MessageFormatter MessageFormatter = new();
-}
